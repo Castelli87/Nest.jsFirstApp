d88a9f03ac0006a043618730f736af849709b467
Castelli87 <davidecastelli87@hotmail.com>
2023-07-26 15:07:46 +0100

    Intoduction to pipes,class-validator into the Dto

--- src/ninjas/dto/create-Ninja.dto.ts
+++ src/ninjas/dto/create-Ninja.dto.ts
@@ -1,5 +1,11 @@
+import { MinLength, IsEnum } from "class-validator";
+
 export class CreateNinjaDto {
+    @MinLength(5)
     name:string;
+
+    @IsEnum(['stars','nunchucks'],{message:'Use correct weapon'})
     weapon:'stars'|'nunchucks'
     
 }
+
