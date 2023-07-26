5f8971b9753f1bccc99b15c2bb42608789cfe44a
Castelli87 <davidecastelli87@hotmail.com>
2023-07-26 11:05:35 +0100

    create the logic in the service/provider

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,13 +1,17 @@
 import { Body, Controller, Get, Param, Post, Put, Query,Delete } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-Ninja.dto';
 import { UpdateNinjaDto } from './dto/update-Ninja.dto';
+import { NinjasService } from './ninjas.service';
 
 @Controller('ninjas')
 export class NinjasController {
-    //filtering by querys
+
+    constructor( private readonly ninjaService: NinjasService){} // injection dependency to instantietion of the service/provider
+
     @Get()
-    getNinjas(@Query('type') type:string){
-        return[{type}]
+    //filtering by querys
+    getNinjas(@Query('weapon') weapon:'stars'|'nunchucks'){
+        return this.ninjaService.getNinjas(weapon)
     }
     @Get(':id')
     getOneNinja(@Param('id')id:string){
